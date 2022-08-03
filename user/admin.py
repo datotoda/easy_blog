f07eeaea12988b73782ebc01d90172556c7d8176
datotoda <datotoda1342@gmail.com>
2022-08-03 23:15:49 +0400

    register User model in admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class EmployeeAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password')
+        }),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'birthdate', 'phone_number', 'image_url')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'password1', 'password2',
+                'first_name', 'last_name', 'birthdate', 'phone_number', 'image_url'),
+        }),
+    )
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('first_name', 'last_name', 'username', 'email')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    ordering = ['username']
