2a722c97b5d58e8909751f54e5d8a73b63224563
datotoda <datotoda1342@gmail.com>
2022-08-04 01:02:51 +0400

    create login, registration, logout and profile views

--- user/views.py
+++ user/views.py
@@ -1,7 +1,51 @@
+from django.conf import settings
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from user.forms import UserRegistrationForm
+from user.models import User
 
 
-def home(request):
-    return HttpResponse('hello')
+def user_profile(request, username):
+    user = User.objects.filter(username=username).first()
+    return render(request, 'user/profile.html', context={'user': user})
+
+
+def user_registration(request):
+    if request.user.is_authenticated:
+        return redirect(settings.LOGIN_REDIRECT_URL)
+
+    form = UserRegistrationForm()
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.image_url = 'https://pngkey.com/png/detail/115-1150152_default-profile-picture-avatar-png-green.png'
+            user.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+
+    return render(request, 'user/registration.html', context={'form': form})
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect(settings.LOGIN_REDIRECT_URL)
+
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect(settings.LOGIN_REDIRECT_URL)
+
+    return render(request, 'user/login.html', context={'form': form})
+
+
+def user_logout(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect(settings.LOGOUT_REDIRECT_URL)
+    return HttpResponse(status=405)
