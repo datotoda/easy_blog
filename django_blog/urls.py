f2755227d0946185f05ef6a230f780682e08c5a9
datotoda <datotoda1342@gmail.com>
2022-08-03 22:21:26 +0400

    register user app urls

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='main-blog')),
+    path('user/', include('user.urls', namespace='main-user')),
 ]
