48e1eb169a752612c98046df71bd46f7c338d50d
datotoda <datotoda1342@gmail.com>
2022-08-03 23:15:37 +0400

    create User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import validate_email
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        validate_email(email)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('birthdate', timezone.now())
+        extra_fields.setdefault('image_url', "https://pbs.twimg.com/profile_images/1387934781514686468/DvGgBAHs_400x400.jpg")
+        extra_fields.setdefault('phone_number', '0')
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    birthdate = models.DateField(verbose_name=_('Birth Date'))
+    image_url = models.URLField(verbose_name=_("Image URL"), blank=True,
+                                default='https://www.pngkey.com/png/detail/115-1150152_default-profile-picture-avatar-png-green.png')
+    phone_number = models.CharField(max_length=50, verbose_name=_('Phone Number'), null=True, blank=True)
+    # saved_posts = models.ManyToManyField(Post)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.get_full_name()
