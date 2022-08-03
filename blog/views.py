a5ccbaea0cb67a3e6b9960b4219841e6c7c80ba5
datotoda <datotoda1342@gmail.com>
2022-08-04 01:01:22 +0400

    change blog:home view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 
 # Create your views here.
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('hello')
+    return render(request, 'home.html')
