fa4a36a0581f973b40fce3d699fe4481b6261158
datotoda <datotoda1342@gmail.com>
2022-08-04 00:59:49 +0400

    change user.urls path

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='main-blog')),
-    path('user/', include('user.urls', namespace='main-user')),
+    path('', include('user.urls', namespace='main-user')),
 ]
