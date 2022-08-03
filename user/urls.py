a92a7295e6490c51a9a93f92e257a7c89ccf2117
datotoda <datotoda1342@gmail.com>
2022-08-04 01:03:05 +0400

    create login, registration, logout and profile urls

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,8 @@ from user import views
 app_name = 'user'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('profile/<str:username>', views.user_profile, name='profile'),
+    path('login/', views.user_login, name='login'),
+    path('registration/', views.user_registration, name='registration'),
+    path('logout/', views.user_logout, name='logout'),
 ]
