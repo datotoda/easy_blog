253c91d6597b556ba618b5d2daf9156754f58629
datotoda <datotoda1342@gmail.com>
2022-08-04 22:50:40 +0400

    register Color, Tag, Post and Comment model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from blog.models import Color, Tag, Post, Comment
 
-# Register your models here.
+
+@admin.register(Color)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('value',)
+    search_fields = ('value',)
+    list_display = ('value',)
+
+
+@admin.register(Tag)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('value', 'color')
+    search_fields = ('value', 'color')
+    list_display = ('value', 'color')
+    fieldsets = (
+        (None,
+         {'fields': ('value', 'color')}
+         ),
+    )
+
+
+@admin.register(Post)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('tags',)
+    search_fields = ('title', 'value')
+    list_display = ('title', 'value')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('user', 'post')
+    search_fields = ('value', 'user', 'post')
+    list_display = ('value', 'user', 'post')
