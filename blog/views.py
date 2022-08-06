bb2c75234ce53be4ef15e45929579b43a62353b7
datotoda <datotoda1342@gmail.com>
2022-08-07 00:52:12 +0400

    Add PostDetailView in blog views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
+
+from blog.models import Post
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post-detail.html'
+    context_object_name = 'post'
