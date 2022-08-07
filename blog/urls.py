8419bc601deb027848a91fa11d581b0b3bbcfeb7
datotoda <datotoda1342@gmail.com>
2022-08-07 21:05:55 +0400

    Add PostCreateView endpoint in blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('post/<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
     path('posts/', views.PostListlView.as_view(), name='post-list'),
+    path('new_post/', views.PostCreateView.as_view(), name='post-add'),
 ]
