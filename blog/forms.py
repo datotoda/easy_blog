5ff86a9c6cc6216e939cc9718782b7d3bd1e5066
datotoda <datotoda1342@gmail.com>
2022-08-07 21:04:37 +0400

    Create PostModelForm in blog forms

---
+++ blog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from blog.models import Post
+
+
+class PostModelForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'value', 'image_url', 'tags')
+
+        widgets = {
+            'title': forms.TextInput(),
+            'value': forms.Textarea(),
+            'image_url': forms.TextInput(),
+            'tags': forms.SelectMultiple(),
+        }
