cd30eb4b39ec6b328585b7d3efb50fb0677ab48d
datotoda <datotoda1342@gmail.com>
2022-08-08 22:29:19 +0400

    Add PostDeleteView endpoint in blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('posts/', views.PostListlView.as_view(), name='post-list'),
     path('new_post/', views.PostCreateView.as_view(), name='post-add'),
     path('post/<slug:slug>/edit', views.PostEditView.as_view(), name='post-edit'),
+    path('post/<slug:slug>/delete', views.PostDeleteView.as_view(), name='post-delete'),
 ]
