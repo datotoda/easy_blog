948b5de01cb6a047f6aabf86d92e068c49897ced
datotoda <datotoda1342@gmail.com>
2022-08-17 22:52:54 +0400

    Create IsSuperUserOrReadOnly permision

--- restapi/permisions.py
+++ restapi/permisions.py
@@ -21,3 +21,14 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.user == request.user
+
+
+class IsSuperUserOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        return False
