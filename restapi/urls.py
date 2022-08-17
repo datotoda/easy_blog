6639eebbb534917e19e68a4c845a85dc06db9854
datotoda <datotoda1342@gmail.com>
2022-08-17 23:03:58 +0400

    Register ColorViewSet, TagViewSet, CommentViewSet and PostViewSet

--- restapi/urls.py
+++ restapi/urls.py
@@ -4,6 +4,10 @@ from restapi import views
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'colors', views.ColorViewSet)
+router.register(r'tags', views.TagViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'posts', views.PostViewSet)
 
 app_name = 'api'
 
