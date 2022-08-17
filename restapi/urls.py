ccc6526f3bc4d16c8c5f16ebfce08bf0f057a46e
datotoda <datotoda1342@gmail.com>
2022-08-17 22:31:06 +0400

    Create Router and add UserViewSet urls

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restapi import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 app_name = 'api'
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
