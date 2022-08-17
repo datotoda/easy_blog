6681aa94be1717c6130cd32cba66269491175097
datotoda <datotoda1342@gmail.com>
2022-08-17 22:30:45 +0400

    Create UserViewSet

--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+import user.models
+from restapi.serializers import UserSerializer
+from restapi import permisions as my_permissions
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = user.models.User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, my_permissions.IsUserOrReadOnly]
+
